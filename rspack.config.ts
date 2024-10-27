00958ca1b1608e1eb313afaa0b5c876c266e8b67
王一之 <yz@ggnb.top>
2024-10-28 01:45:38 +0800

    config

--- rspack.config.ts
+++ rspack.config.ts
@@ -1,16 +1,18 @@
+import * as path from "path";
 import { defineConfig } from "@rspack/cli";
 import { rspack } from "@rspack/core";
 import * as RefreshPlugin from "@rspack/plugin-react-refresh";
+import { version } from "./package.json";
 
 const isDev = process.env.NODE_ENV === "development";
+const isBeta = version.includes("-");
 
 // Target browsers, see: https://github.com/browserslist/browserslist
 const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
 
 const src = `${__dirname}/src`;
 const dist = `${__dirname}/dist`;
-const assets = `${__dirname}/build/assets`;
-const template = `${assets}/template`;
+const assets = `${src}/assets`;
 
 export default defineConfig({
   context: __dirname,
@@ -18,8 +20,17 @@ export default defineConfig({
     service_worker: `${src}/service_worker.ts`,
     popup: `${src}/pages/popup/main.tsx`,
   },
+  output: {
+    path: `${dist}/ext/src`,
+    filename: "[name].js",
+    clean: true,
+  },
   resolve: {
     extensions: ["...", ".ts", ".tsx", ".jsx"],
+    alias: {
+      "@App": path.resolve(__dirname, "src/"),
+      "@Pkg": path.resolve(__dirname, "pkg/"),
+    },
   },
   module: {
     rules: [
@@ -54,6 +65,29 @@ export default defineConfig({
     ],
   },
   plugins: [
+    new rspack.CopyRspackPlugin({
+      patterns: [
+        {
+          from: `${src}/manifest.json`,
+          to: `${dist}/ext`,
+          // 将manifest.json内版本号替换为package.json中版本号
+          transform(content) {
+            const manifest = JSON.parse(content.toString());
+            manifest.name = "ScriptCat - Dev";
+            manifest.content_security_policy = "script-src 'self' https://cdn.crowdin.com; object-src 'self'";
+            return JSON.stringify(manifest);
+          },
+        },
+        {
+          from: `${assets}/logo/logo${isDev ? "-beta" : ""}.png`,
+          to: `${dist}/ext/assets/logo.png`,
+        },
+        {
+          from: `${assets}/_locales`,
+          to: `${dist}/ext/src/_locales`,
+        },
+      ],
+    }),
     new rspack.HtmlRspackPlugin({
       filename: `${dist}/ext/src/popup.html`,
       template: `${src}/pages/popup/index.html`,
