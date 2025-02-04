1b869e80590bdc854e097fa49a1853c3bb5ac108
王一之 <yz@ggnb.top>
2025-02-05 00:16:01 +0800

    logger

--- src/pages/install/main.tsx
+++ src/pages/install/main.tsx
@@ -7,8 +7,21 @@ import "@App/locales/locales";
 import "@App/index.css";
 import { Provider } from "react-redux";
 import { store } from "@App/pages/store/store.ts";
+import LoggerCore from "@App/app/logger/core.ts";
+import migrate from "@App/app/migrate.ts";
+import { LoggerDAO } from "@App/app/repo/logger.ts";
+import DBWriter from "@App/app/logger/db_writer.ts";
 
+// 初始化数据库
+migrate();
+// 初始化日志组件
+const loggerCore = new LoggerCore({
+  debug: process.env.NODE_ENV === "development",
+  writer: new DBWriter(new LoggerDAO()),
+  labels: { env: "install" },
+});
 
+loggerCore.logger().debug("page start");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
