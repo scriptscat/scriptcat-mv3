48f1b1f33b0e1935643f783ded8a4179df7f8b6c
王一之 <yz@ggnb.top>
2025-03-30 22:58:12 +0800

    content

--- src/content.ts
+++ src/content.ts
@@ -2,6 +2,7 @@ import LoggerCore from "./app/logger/core";
 import MessageWriter from "./app/logger/message_writer";
 import { SandboxManager } from "./app/service/sandbox";
 import { ExtensionMessageSend } from "@Packages/message/extension_message";
+import ContentRuntime from "./runtime/content/content";
 
 function main() {
   // 建立与service_worker页面的连接
@@ -10,13 +11,14 @@ function main() {
   // 初始化日志组件
   const loggerCore = new LoggerCore({
     writer: new MessageWriter(msg),
-    labels: { env: "sandbox" },
+    labels: { env: "content" },
   });
-  loggerCore.logger().debug("offscreen start");
+  loggerCore.logger().debug("content start");
 
-  // 初始化管理器
-  const manager = new SandboxManager(msg);
-  manager.initManager();
+  // 初始化运行环境
+  const contentMessage = new MessageContent(scriptFlag, true);
+  const runtime = new ContentRuntime(contentMessage, internalMessage);
+  runtime.start();
 }
 
 main();
